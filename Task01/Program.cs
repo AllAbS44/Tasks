7a5189ea35b8c1a512e5718b9fc8db116ef3e40c
Stroganov Ivan <gangun.4444@gmail.com>
2023-10-01 01:31:19 +0300

    Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите переменную a: ");
+double a = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите переменную b: ");
+double b = double.Parse(Console.ReadLine());
+
+if (b * b == a) Console.WriteLine($"a = {a}, b = {b} -> Да");
+else Console.WriteLine($"a = {a}, b = {b} -> Нет");
