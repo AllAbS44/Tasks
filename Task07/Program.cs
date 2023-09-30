694d47edaf83689db98b821996b96a99eee3a0af
Stroganov Ivan <gangun.4444@gmail.com>
2023-10-01 02:07:17 +0300

    Task07

---
+++ Task07/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите трёхзначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+if ((number < 1000 && number > 99) || (number < -99 && number > -1000))
+{
+    int x = number % 100;
+    x %= 10;
+    Console.WriteLine($"{number} -> {x}");
+}
+else Console.WriteLine("Число не трёхзначное :)");
