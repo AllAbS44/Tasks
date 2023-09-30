70f597e47450c7e714fd0763bdef3b3a1a1bcde5
Stroganov Ivan <gangun.4444@gmail.com>
2023-10-01 02:07:00 +0300

    Task05

---
+++ Task05/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите целое число N: ");
+int N = int.Parse(Console.ReadLine());
+
+if (N > 0)
+{
+    int count = -N;
+    Console.Write($"{N} -> ");
+    while (count < N + 1)
+    {
+        Console.Write($"{count} ");
+        count += 1;
+    }
+}
+else if (N < 0)
+{
+    int count = -N;
+    Console.Write($"{N} -> ");
+    while (count > N - 1)
+    {
+        Console.Write($"{-count} ");
+        count -= 1;
+    }
+}
+else Console.WriteLine($"{N} -> {N}");
