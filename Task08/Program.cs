b9d1b3909c579add0555b244b1ec1608a0738cbb
Stroganov Ivan <gangun.4444@gmail.com>
2023-10-01 02:57:02 +0300

    Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите целое число больше единицы: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number < 2) Console.WriteLine("Не подходит по условию :)");
+else if (number >= 2)
+{
+    Console.Write($"{number} -> ");
+
+    int count = 1;
+
+    while (count < number + 1)
+    {
+        if (count % 2 == 0) Console.Write($"{count} ");
+        count += 1;
+    }
+}
