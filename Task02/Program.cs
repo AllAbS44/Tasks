67e22e8693b1aec503fa662ea2194dfd3207283e
Stroganov Ivan <gangun.4444@gmail.com>
2023-10-01 02:33:20 +0300

    Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a > b) Console.WriteLine($"a = {a}; b = {b} -> max = {a}");
+else if (a < b) Console.WriteLine($"a = {a}; b = {b} -> max = {b}");
+else Console.WriteLine($"a = {a}; b = {b} -> max = {a}");
