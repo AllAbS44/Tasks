df10370b4ee6e8c381e5efc3f7b00f059fd5836a
Stroganov Ivan <gangun.4444@gmail.com>
2023-10-01 01:31:04 +0300

    Task00

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите целое число: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"{number} -> {number * number}");
