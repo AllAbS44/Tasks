eeb2a88c8f37446666dc04edd231e3f71449ae8b
Stroganov Ivan <gangun.4444@gmail.com>
2023-10-07 23:01:53 +0300

    Task12

---
+++ Task12/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+void Multiple(int num1, int num2)
+{
+    if (num1 % num2 == 0) Console.WriteLine($"{num1}, {num2} -> кратно");
+    else Console.WriteLine($"{num1}, {num2} -> не кратно, остаток {num1 % num2}");
+}
+
+Multiple(number1, number2);
