0f4989c6027b018f781c80545dcac71b0178895b
Stroganov Ivan <gangun.4444@gmail.com>
2023-10-08 00:32:41 +0300

    Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.Write("Введите первый делитель: ");
+int divider1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второй делитель: ");
+int divider2 = int.Parse(Console.ReadLine());
+
+void Multiple(int numb, int div1, int div2)
+{
+    if ((numb % div1 == 0) && (number % div2 == 0)) Console.WriteLine($"{numb} одновременно кратен {div1} и {div2}");
+    else Console.WriteLine($"{numb} не кратен {div1} и {div2} одновременно");
+}
+
+Multiple(number, divider1, divider2);
