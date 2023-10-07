143eed7a12bc9b3ada5f5cb95477320193ba6f6d
Stroganov Ivan <gangun.4444@gmail.com>
2023-10-08 01:03:21 +0300

    Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,11 @@
+﻿int SecondDigit(int numb)
+{
+    return (numb % 100) / 10;
+}
+
+Console.Write("Введите трёхзначное число: ");
+int number1 = int.Parse(Console.ReadLine());
+
+if ((number1 < 1000) && (number1 > 99)) Console.WriteLine($"{number1} -> {SecondDigit(number1)}");
+else if ((number1 < -99) && (number1 > -1000)) Console.WriteLine($"{number1} -> {SecondDigit(number1) * -1}");
+else Console.WriteLine("Число не трёхзначное");
