eeda139cedae4c275a7b34783e0153a31169c52e
Stroganov Ivan <gangun.4444@gmail.com>
2023-10-07 22:40:54 +0300

    Task10

---
+++ Task11/Program.cs
@@ -0,0 +1,11 @@
+﻿int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное трёхзначное число -> {number}");
+
+int DelSecond(int numb)
+{
+    int firstDigit = (numb / 100);
+    int lastDigit = numb % 10;
+    return firstDigit * 10 + lastDigit;
+}
+
+Console.WriteLine($"Двухзначное число -> {DelSecond(number)}");
