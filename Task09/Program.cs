a51b76fb9cbdfea838ebf08fe6d60ed9132334b7
Stroganov Ivan <gangun.4444@gmail.com>
2023-10-07 22:25:43 +0300

    Task09

---
+++ Task09/Program.cs
@@ -0,0 +1,17 @@
+﻿int number = new Random().Next(10, 100);
+
+Console.WriteLine($"Случайное числа из отрезка [10, 99] -> {number}");
+
+// int FirstDigit = number / 10;
+// int SecondDigit = number % 10;
+// if (FirstDigit > SecondDigit) Console.WriteLine($"Наибольшая цифра числа -> {FirstDigit}");
+// else Console.WriteLine($"Наибольшая цифра числа -> {SecondDigit}");
+
+int MaxDigit(int num)
+{
+    int FirstDigit = num / 10;
+    int SecondDigit = num % 10;
+    return FirstDigit > SecondDigit ? FirstDigit : SecondDigit;
+}
+
+Console.WriteLine($"Наибольшая цифра числа -> {MaxDigit(number)}");
