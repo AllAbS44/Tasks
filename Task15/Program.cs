90d7a6bb14ea3f4be86a5f6b7578fae661f3c5a0
Stroganov Ivan <gangun.4444@gmail.com>
2023-10-08 01:38:26 +0300

    Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите номер дня недели: ");
+int number = int.Parse(Console.ReadLine());
+
+if ((number < 1) || (number > 7)) Console.WriteLine("Нет такого дня недели");
+else if ((number >= 1) && (number <= 5)) Console.WriteLine($"{number} -> не выходной");
+else Console.WriteLine($"{number} -> выходной");
