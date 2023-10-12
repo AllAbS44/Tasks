441ad24bca78a60c4961c347ec7246563dccabc3
Stroganov Ivan <gangun.4444@gmail.com>
2023-10-12 18:55:20 +0300

    Task17

---
+++ Task17/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите координаты точек");
+
+Console.Write("X: ");
+int xCoordinate = int.Parse(Console.ReadLine());
+
+Console.Write("Y: ");
+int yCoordinate = int.Parse(Console.ReadLine());
+
+int quarter = Quarter(xCoordinate, yCoordinate);
+
+string result = quarter > 0 
+                ? $"Указанные координаты соответствуют четверти {quarter}" 
+                : "Необходимо ввести координаты отличные от нуля";
+
+Console.WriteLine(result);
+
+int Quarter(int x, int y)
+{
+    if (x > 0 && y > 0) return 1;
+    if (x < 0 && y > 0) return 2;
+    if (x < 0 && y < 0) return 3;
+    if (x > 0 && y < 0) return 4;
+    return 0;
+}
