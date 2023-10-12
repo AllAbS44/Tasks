1d75795b78b235fe95742a45f301130579ab6293
Stroganov Ivan <gangun.4444@gmail.com>
2023-10-12 19:50:36 +0300

    Task22

---
+++ Task22/Program.cs
@@ -0,0 +1,10 @@
+﻿void TableSquare(int number) 
+{
+    for (int i = 1; i < number + 1; i++) Console.WriteLine($"{i, 3} -> {Math.Pow(i, 2), 5}");
+}
+
+Console.WriteLine("Введите число N");
+Console.Write("N: ");
+int N = int.Parse(Console.ReadLine());
+
+TableSquare(N);
