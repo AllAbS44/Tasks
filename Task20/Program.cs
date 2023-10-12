ff7970c53ea99d670b37d6239e7864bc51377f6b
Stroganov Ivan <gangun.4444@gmail.com>
2023-10-12 19:32:16 +0300

    Task20

---
+++ Task20/Program.cs
@@ -0,0 +1,23 @@
+﻿double PointsDistance(int x1, int y1, int x2, int y2)
+{
+    double X = Math.Pow((x2 - x1), 2);
+    double Y = Math.Pow((y2 - y1), 2);
+    double distance = Math.Sqrt (X + Y);
+    return Math.Round(distance, 2, MidpointRounding.ToZero);
+}
+
+Console.WriteLine("Введите координаты точки А");
+Console.Write("x: ");
+int xA = int.Parse(Console.ReadLine());
+Console.Write("y: ");
+int yA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки B");
+Console.Write("x: ");
+int xB = int.Parse(Console.ReadLine());
+Console.Write("y: ");
+int yB = int.Parse(Console.ReadLine());
+
+double result = PointsDistance(xA, yA, xB, yB);
+
+Console.WriteLine($"A ({xA},{yA}); B({xB},{yB}) -> {result}");
