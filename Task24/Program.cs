6bdb454cc80d81e073caba5c21ffcfd17a4562ce
Stroganov Ivan <gangun.4444@gmail.com>
2023-10-13 20:12:01 +0300

    Task24

---
+++ Task24/Program.cs
@@ -0,0 +1,15 @@
+﻿int Sum(int number)
+{
+    int sum = 0;
+    for (int i = 1; i < number + 1; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите целое положительное число");
+Console.Write("A: ");
+int A = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{A} -> {Sum(A)}");
