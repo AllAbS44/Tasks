ccbb0316443908dc220b8a03978f6c94a2d9dd68
Stroganov Ivan <gangun.4444@gmail.com>
2023-10-13 20:27:08 +0300

    Task28

---
+++ Task28/Program.cs
@@ -0,0 +1,15 @@
+﻿int Multiply(int number)
+{
+    int mult = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        mult *= i;
+    }
+    return mult;
+}
+
+Console.WriteLine("Введите целое положительное число");
+Console.Write("N: ");
+int N = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{N} -> {Multiply(N)}");
