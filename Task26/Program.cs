a01ba3cbf3737a478384feff39b33576d5a2d8ba
Stroganov Ivan <gangun.4444@gmail.com>
2023-10-13 20:19:06 +0300

    Task26

---
+++ Task26/Program.cs
@@ -0,0 +1,16 @@
+﻿int CountOfNumbers(int numb)
+{
+    int count = 0;
+    while (numb != 0)
+    {
+        numb /= 10;
+        count += 1;
+    }
+    return count;
+}
+
+Console.WriteLine("Введите число");
+Console.Write("Number: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{number} -> {CountOfNumbers(number)}");
