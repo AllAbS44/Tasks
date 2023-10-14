c070cb5d9c31e2575cf686c0101bde6c81f32bc0
Stroganov Ivan <gangun.4444@gmail.com>
2023-10-15 01:02:05 +0300

    Task35

---
+++ Task35/Program.cs
@@ -0,0 +1,44 @@
+﻿int[] RandomArray (int min, int max, int length)
+{
+    int[] arr = new int[length];
+    var rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] mas)
+{
+    int i = 0;
+    while (i < mas.Length - 1)
+    {
+        Console.Write(mas[i] + ", ");
+        i++;
+    }
+    Console.Write(mas[i]);
+}
+
+int CountInInterval(int[] arr, int min, int max)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= min && arr[i] <= max) count++;
+    }
+    return count;
+}
+
+Console.Write("Введите длину массива: ");
+int length = int.Parse(Console.ReadLine());
+
+int[] array = RandomArray(-100, 100, length);
+
+int countInInterval = CountInInterval(array, 10, 99);
+
+Console.Write("[");
+PrintArray(array);
+Console.Write("]");
+
+Console.Write($" -> {countInInterval}");
