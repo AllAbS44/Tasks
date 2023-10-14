06cde6cba60dd8adb580a54755ffbffa9d37d941
Stroganov Ivan <gangun.4444@gmail.com>
2023-10-15 00:42:43 +0300

    Task33

---
+++ Task33/Program.cs
@@ -0,0 +1,47 @@
+﻿int[] RandomArray (int min, int max, int length)
+{
+    int[] arr = new int[length];
+    var rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] mas)
+{
+    int i = 0;
+    while (i < mas.Length - 1)
+    {
+        Console.Write(mas[i] + ", ");
+        i++;
+    }
+    Console.Write(mas[i]);
+}
+
+bool FindNumber(int[] mas, int num)
+{
+    for (int i = 0; i < mas.Length; i++)
+    {
+        if (mas[i] == num) return true;
+    }
+    return false;
+}
+
+Console.Write("Введите длину массива: ");
+int length = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int[] array = RandomArray(-15, 15, length);
+
+Console.Write("[");
+PrintArray(array);
+Console.Write("]");
+Console.WriteLine();
+
+bool findNumber = FindNumber(array, number);
+
+Console.WriteLine(findNumber ? "Число найдено" : "Число не найдено");
