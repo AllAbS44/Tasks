20ff70da202c32f13570e196d573b9725fbc20f0
Stroganov Ivan <gangun.4444@gmail.com>
2023-10-15 01:36:18 +0300

    Task36

---
+++ Task36/Program.cs
@@ -0,0 +1,43 @@
+﻿int[] RandomArray (int min, int max, int length)
+{
+    int[] arr = new int[length];
+    var rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] mas)
+{
+    int i = 0;
+    while (i < mas.Length - 1)
+    {
+        Console.Write(mas[i] + ", ");
+        i++;
+    }
+    Console.Write(mas[i]);
+}
+
+int SumOfElemInOddPos(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 != 0) sum += arr[i];
+    }
+    return sum;
+}
+
+Console.Write("Введите длину массива: ");
+int length = int.Parse(Console.ReadLine());
+
+int[] array = RandomArray(-100, 100, length);
+
+Console.Write("[");
+PrintArray(array);
+Console.Write("]");
+Console.Write(" -> ");
+
+Console.WriteLine(SumOfElemInOddPos(array));
