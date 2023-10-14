dbd59d2c9d9b8ce0185c18852f615e829235a579
Stroganov Ivan <gangun.4444@gmail.com>
2023-10-14 12:25:12 +0300

    Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,17 @@
+﻿int Degree(int A, int B)
+{
+    int number = 1;
+    for (int i = 0; i < B; i++)
+    {
+        number *= A;
+    }
+    return number;
+}
+
+Console.Write("Введите целое число A: ");
+int A = int.Parse(Console.ReadLine());
+
+Console.Write("Введите целое положительное число B: ");
+int B = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{A}, {B} -> {Degree(A, B)}");
