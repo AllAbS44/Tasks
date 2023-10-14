4883d8c7afd0f65bfe02717d3c7f9ce2c09bea6f
Stroganov Ivan <gangun.4444@gmail.com>
2023-10-14 12:37:06 +0300

    Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,15 @@
+﻿int SumNumber(int numb)
+{
+    int count = 0;
+    while (numb != 0)
+    {
+        count += numb % 10;
+        numb /= 10;
+    }
+    return count;
+}
+
+Console.Write("Введите целое число: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{number} -> {SumNumber(number)}");
