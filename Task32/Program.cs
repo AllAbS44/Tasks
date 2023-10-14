2c8409271582986f9aabc7b210ce3c1344c73d58
Stroganov Ivan <gangun.4444@gmail.com>
2023-10-14 22:58:43 +0300

    Task32

---
+++ Task32/Program.cs
@@ -0,0 +1,46 @@
+﻿int[] RandomArray (int min, int max, int length)
+{
+    int[] arr = new int[length];
+    var rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] mas)
+{
+    int i = 0;
+    while (i < mas.Length - 1)
+    {
+        Console.Write(mas[i] + ", ");
+        i++;
+    }
+    Console.Write(mas[i]);
+}
+
+void InversArray(int[] mas)
+{
+    for (int i = 0; i < mas.Length; i++)
+    {
+        mas[i] = -mas[i];
+    }
+}
+
+Console.Write("Введите длину массива: ");
+int length = int.Parse(Console.ReadLine());
+
+int[] array = RandomArray(-15, 15, length);
+
+Console.Write("[");
+PrintArray(array);
+Console.Write("]");
+Console.WriteLine();
+
+InversArray(array);
+
+Console.Write("[");
+PrintArray(array);
+Console.Write("]");
+Console.WriteLine();
