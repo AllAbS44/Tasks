55c7f189ff28bd516e9c1bca6b6ba41c795ed874
Stroganov Ivan <gangun.4444@gmail.com>
2023-10-21 03:55:21 +0300

    Task42

---
+++ Task42/Program.cs
@@ -0,0 +1,19 @@
+﻿int DecToBin(int num)
+{
+    int res = 0;
+    int i = 1;
+    while(num > 0)
+    {
+        res += num % 2 * i;
+        num /= 2;
+        i *= 10;
+    }
+    return res;
+}
+
+Console.WriteLine("Введите целое число: ");
+int number = int.Parse(Console.ReadLine());
+
+int result = DecToBin(number);
+
+Console.WriteLine(result);
