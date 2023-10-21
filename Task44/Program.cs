65269b5d00f1ea8d0f0c87a544f3c2af731c96b0
Stroganov Ivan <gangun.4444@gmail.com>
2023-10-21 04:10:54 +0300

    Task44

---
+++ Task44/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] FiboArray(int num)
+{
+    int[] arr = new int[num];
+    arr[1] = 1;
+    for (int i = 2; i < arr.Length; i++)
+    {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+    return arr;
+}
+
+void PrintArray(int[] mas)
+{
+    int i = 0;
+    while (i < mas.Length - 1)
+    {
+        Console.Write(mas[i] + " ");
+        i++;
+    }
+    Console.Write(mas[i]);
+}
+
+Console.WriteLine("Введите целое число больше единицы: ");
+int length = int.Parse(Console.ReadLine());
+
+if(length < 2)
+{
+    Console.WriteLine("Некорректный ввод.");
+    return;
+}
+
+int[] array = FiboArray(length);
+
+Console.Write($"Если N = {length} -> ");
+PrintArray(array);
