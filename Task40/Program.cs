8b0f820a577ec65603b1f41373bc57a35e9fb98b
Stroganov Ivan <gangun.4444@gmail.com>
2023-10-21 03:43:37 +0300

    Task40

---
+++ Task40/Program.cs
@@ -0,0 +1,19 @@
+﻿bool IsExistTriangle(int first, int second, int third)
+{
+    return first < second + third
+        && second < first + third
+        && third < first + second;
+}
+
+Console.Write("Введите длину первой стороны: ");
+int length1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите длину второй стороны: ");
+int length2 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите длину третьей стороны: ");
+int length3 = int.Parse(Console.ReadLine());
+
+bool result = IsExistTriangle(length1, length2, length3);
+
+Console.WriteLine(result ? "Такой треугольник существует" : "Такого треугольника не существует");
